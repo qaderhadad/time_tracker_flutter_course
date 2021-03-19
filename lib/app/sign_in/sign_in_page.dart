3703d0fc6ca5f5ddf6a6de78005834a53ad48a72
AbdulQader <abedhadad998@gmail.com>
2021-03-20 01:00:27 +0200

    Auth Class fully implemented

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,10 +1,23 @@
-import 'package:flutter/cupertino.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_button.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/social_sign_in_button.dart';
-import 'package:time_tracker_flutter_course/common_widgets/custom_raised_button.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class SignInPage extends StatelessWidget {
+  const SignInPage({Key key, @required this.auth, @required this.onSignIn}) : super(key: key);
+  final AuthBase auth;
+  final void Function(User) onSignIn;
+
+  Future<void> _signInAnonymously() async {
+    try {
+      final user = await auth.signInAnonymously();
+      onSignIn(user);
+    } catch (e) {
+      print(e.toString);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,62 +29,62 @@ class SignInPage extends StatelessWidget {
       backgroundColor: Colors.grey[200],
     );
   }
-}
 
-Widget _buildContent() {
-  //underscore(_) = Private, accessible only at File Level
-  return Padding(
-    padding: EdgeInsets.all(16), //Padding from the sides
-    child: Column(
-      // Takes a list of widgets
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          "Sign In",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 32.0,
-            fontWeight: FontWeight.w600,
+  Widget _buildContent() {
+    //underscore(_) = Private, accessible only at File Level
+    return Padding(
+      padding: EdgeInsets.all(16), //Padding from the sides
+      child: Column(
+        // Takes a list of widgets
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Sign In",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w600,
+            ),
           ),
-        ),
-        SizedBox(height: 48.0), //SizedBox to add padding between Containers
-        SocialSignInButton(
-          assetName: 'images/google-logo.png',
-          text: 'Sign in with Google',
-          textColor: Colors.black87,
-          color: Colors.white,
-          onPressed: () {},
-        ),
-        SizedBox(height: 8.0),
-        SocialSignInButton(
-          assetName: 'images/facebook-logo.png',
-          text: 'Sign in with Facebook',
-          textColor: Colors.white,
-          color: Color(0xFF334D92),
-          onPressed: () {},
-        ),
-        SizedBox(height: 8.0),
-        SignInButton(
-          text: 'Sign in with email',
-          textColor: Colors.white,
-          color: Colors.teal[700],
-          onPressed: () {},
-        ),
-        SizedBox(height: 8.0),
-        Text(
-          'or',
-          style: TextStyle(fontSize: 14.0, color: Colors.black87),
-          textAlign: TextAlign.center,
-        ),
-        SizedBox(height: 8.0),
-        SignInButton(
-          text: 'Go anonymous',
-          textColor: Colors.black,
-          color: Colors.lime[300],
-          onPressed: () {},
-        ),
-      ],
-    ),
-  );
+          SizedBox(height: 48.0), //SizedBox to add padding between Containers
+          SocialSignInButton(
+            assetName: 'images/google-logo.png',
+            text: 'Sign in with Google',
+            textColor: Colors.black87,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          SocialSignInButton(
+            assetName: 'images/facebook-logo.png',
+            text: 'Sign in with Facebook',
+            textColor: Colors.white,
+            color: Color(0xFF334D92),
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Sign in with email',
+            textColor: Colors.white,
+            color: Colors.teal[700],
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            'or',
+            style: TextStyle(fontSize: 14.0, color: Colors.black87),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Go anonymous',
+            textColor: Colors.black,
+            color: Colors.lime[300],
+            onPressed: _signInAnonymously,
+          ),
+        ],
+      ),
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import "package:flutter/material.dart";
-import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/landing_page.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
 
 //Entry Point for the app
 void main() async {  // (async) bc we're using an Asynchronous method (await)
@@ -18,7 +19,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo
     ),
-      home: SignInPage(),
+      home: LandingPage(
+        auth: Auth(),
+      ),
     );
   }
 }
