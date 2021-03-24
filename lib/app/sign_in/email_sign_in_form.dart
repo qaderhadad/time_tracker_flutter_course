d8ea11afb6ee1e468ddfa9f91b3908567543dbeb
AbdulQader <abedhadad998@gmail.com>
2021-03-24 19:54:44 +0200

    Fixed Crashing Issue

---
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/common_widgets/form_submit_button.dart';
+
+class EmailSignInForm extends StatelessWidget {
+
+  List<Widget> _buildChildren() {
+    return [
+      TextField(
+        decoration: InputDecoration(
+          labelText: 'Email',
+          hintText: 'test@test.com',
+        ),
+      ),
+      SizedBox(height: 8.0),
+      TextField(
+        decoration: InputDecoration(
+          labelText: 'Password',
+        ),
+        obscureText: true,
+      ),
+      RaisedButton(
+        child: Text('Sign in'),
+        onPressed: () {},
+      ),
+      SizedBox(height: 8.0),
+      FlatButton(
+          child: Text('Need an account? Register Habibi'),
+          onPressed: () {},
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
+        children: _buildChildren(),
+      ),
+    );
+  }
+}
