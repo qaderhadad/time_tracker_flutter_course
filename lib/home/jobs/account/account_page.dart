9d8e212de4732df989273187e5461af2dfcb2451
AbdulQader <abedhadad998@gmail.com>
2021-04-01 01:23:43 +0300

    Last Commit. App Complete

--- lib/home/jobs/account/account_page.dart
+++ lib/home/jobs/account/account_page.dart
@@ -1,7 +1,9 @@
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter_course/common_widgets/avatar.dart';
 import 'package:time_tracker_flutter_course/common_widgets/show_alert_dialog.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
@@ -29,6 +31,7 @@ class AccountPage extends StatelessWidget {
   }
   @override
   Widget build(BuildContext context) {
+    final auth = Provider.of<AuthBase>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('Account'),
@@ -44,7 +47,29 @@ class AccountPage extends StatelessWidget {
             onPressed: () => _confirmSignOut(context),
           ),
         ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(130),
+          child:  _buildUserInfo(auth.currentUser),
+        )
       ),
+    );
+  }
+
+  Widget _buildUserInfo(User user) {
+    return Column(
+      children: [
+        Avatar(
+          photoUrl: user.photoURL,
+          radius: 50,
+        ),
+        SizedBox(height: 8),
+        if(user.displayName != null)
+          Text(
+            user.displayName,
+            style: TextStyle(color: Colors.white),
+          ),
+        SizedBox(height: 8),
+      ],
     );
   }
 }
